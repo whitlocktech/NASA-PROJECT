5afda01a74b7acaa9938195a896a56f4dc687a7b
whitlocktech <whitlocktech@gmail.com>
2023-04-09 04:11:28 -0500

    implemented Morgan

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path')
 const planetsRouter = require('./routes/planets/planets.router')
+const morgan = require('morgan')
 
 
 const app = express()
@@ -9,6 +10,8 @@ const app = express()
 app.use(cors({
     origin: 'http://localhost:3000',
 }))
+
+app.use(morgan('combined'))
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '..', 'public')))
